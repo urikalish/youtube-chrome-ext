d4d8d8d63a9cd391c34dd61b65e1a9caf07132e3
urikalish <uri.kalish@gmail.com>
2019-03-17 03:32:02 +0200

    wip

--- youtube-ext.js
+++ youtube-ext.js
@@ -3,46 +3,75 @@ console.log('YouTubeChromeExt');
 
 var videosData = {};
 
+function observeDOM(obj, cb){
+	let obs = new MutationObserver((mutations) => {
+	if (mutations[0].addedNodes.length || mutations[0].removedNodes.length )
+		cb();
+	});
+	obs.observe(
+		obj,
+		{
+			childList: true,
+			subtree:true
+		}
+	);
+}
+
 setTimeout(function() {
 	getAllVideosData();
+	observeDOM(document.querySelector('div#items.ytd-watch-next-secondary-results-renderer'), () => {
+		getAllVideosData();
+	});
 }, 5000);
 
 function getAllVideosData() {
 	let elms = document.querySelectorAll('div#items.ytd-watch-next-secondary-results-renderer ytd-compact-video-renderer');
 	for (let i = 0; i < elms.length; i++) {
 		let videoId = elms[i].querySelector('a#thumbnail').getAttribute('href').substring(9);
-		videosData[videoId] = {
-			id: videoId,
-			parentDomElm: elms[i]
-		};
-		getVideoData(videoId);	
+		if (!videosData[videoId]) {
+			videosData[videoId] = {
+				id: videoId,
+				parentDomElm: elms[i]
+			};
+			getVideoData(videoId);
+		}
 	}		
 }
 
 function getVideoData(id) {
-	let url = `https://www.googleapis.com/youtube/v3/videos?part=statistics&key=AIzaSyDC3ZkKLUZ4rhoJbyTGxiD3YKmokimspXU&id=${id}`;
-	let xhr = new XMLHttpRequest();
-	xhr.onreadystatechange = function() {
-		if (xhr.readyState === 4) {
-			let data = JSON.parse(xhr.response);
-			handleVideoData(data);			
-		}
-	};
-	xhr.onerror = function() {console.log(`error on httpGet() - ${err}`);};
-	xhr.open('GET', url, true);
-	xhr.send('');	
+	try {
+		let xhr = new XMLHttpRequest();	
+		xhr.onreadystatechange = () => {
+			if (xhr.readyState === 4) {
+				let data = JSON.parse(xhr.response);
+				handleVideoData(data);			
+			}
+		};
+		xhr.onerror = () => {
+			console.log(`error on getVideoData() - ${err}`);
+		};
+		let url = `https://www.googleapis.com/youtube/v3/videos?part=statistics&key=AIzaSyDC3ZkKLUZ4rhoJbyTGxiD3YKmokimspXU&id=${id}`;
+		xhr.open('GET', url, true);
+		xhr.send('');	
+	} catch(err) {
+		console.log(`error on getVideoData() - ${err}`)	
+	}
 }
 
 function handleVideoData(data) {
-	let id = data.items[0].id;
-	let stats = data.items[0].statistics;
-	let videoData = videosData[id];
-	videoData.viewCount = stats.viewCount;
-	videoData.likeCount = stats.likeCount;
-	videoData.dislikeCount = stats.dislikeCount;
-	videoData.favoriteCount = stats.favoriteCount;
-	videoData.commentCount = stats.commentCount;
-	updateDom(videoData);
+	if (data && data.items && data.items[0]) {
+		let id = data.items[0].id;
+		let stats = data.items[0].statistics;
+		let videoData = videosData[id];
+		videoData.viewCount = stats.viewCount;
+		videoData.likeCount = stats.likeCount;
+		videoData.dislikeCount = stats.dislikeCount;
+		videoData.favoriteCount = stats.favoriteCount;
+		videoData.commentCount = stats.commentCount;
+		updateDom(videoData);
+	} else {
+		console.log(`error on handleVideoData() - missing data object`)
+	}
 }
 
 function updateDom(videoData) {
